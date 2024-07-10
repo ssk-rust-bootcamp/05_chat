fef59f58f83ea300409cb5fbaa64e1fc4fb219a3
shikun.sh <shikun.sh@cheilpengtai.com>
2024-07-10 19:28:17 +0800

    v5-7-jwt-signin-signup

--- chat_server/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum AppError {
-    #[error("sql error :{0}")]
-    SqlxError(#[from] sqlx::Error),
-    #[error("password hash error :{0}")]
-    PasswordHashError(#[from] argon2::password_hash::Error),
-}
--- migrations/20240709013020_initial.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE if EXISTS messages;
-DROP TABLE if EXISTS users;
-DROP TABLE if EXISTS chats;
-DROP type if EXISTS chat_type;
